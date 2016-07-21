aedcdca9689544d98131e3caef7b8ee08212e9ae
misoton665 <misoton998@hotmail.co.jp>
2016-07-21 17:42:12 +0900

    Add some comment

--- app/src/main/java/org/misoton/exampleapp/SubActivity.java
+++ app/src/main/java/org/misoton/exampleapp/SubActivity.java
@@ -19,14 +19,19 @@ public class SubActivity extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
+        // 投稿したメッセージを表示するListView
         final ListView messageListView = (ListView) this.findViewById(R.id.sub_message_list);
         assert messageListView != null;
+
+        // メッセージの文字列を管理するArrayAdapter
         messageAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1);
         messageListView.setAdapter(messageAdapter);
 
+        // メッセージを入力するEditText
         messageEdit = (EditText) this.findViewById(R.id.sub_message_edit);
         assert messageEdit != null;
 
+        // メッセージを投稿するButton
         final Button submitButton = (Button) this.findViewById(R.id.sub_submit_button);
         assert submitButton != null;
         submitButton.setOnClickListener(this);
@@ -37,11 +42,16 @@ public class SubActivity extends AppCompatActivity implements View.OnClickListen
     public void onClick(@NonNull View v) {
         switch (v.getId()) {
             case R.id.sub_submit_button:
+                // メッセージを取得、空なら何もしない
                 String message = messageEdit.getText().toString();
                 if (message.length() == 0) {
                     return;
                 }
+
+                // メッセージをリストに追加
                 messageAdapter.add(message);
+
+                // 入力されたメッセージを消去
                 messageEdit.setText("");
                 break;
         }
